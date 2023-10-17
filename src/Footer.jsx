aed158e4f39152fad82291492993e10888b2f9b5
jvin08 <jvin08@gmail.com>
2023-10-17 10:40:38 -0500

    social icons path update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,10 @@
 export default function Footer() {
     return (
         <div className="social">
-            <a href="https://twitter.com/jvin08" title="twitter"><img src="./public/xtw.JPG"/></a>
-            <a href="https://www.facebook.com/Djon.Pypkin/"title="meta"><img src="./public/f.JPG"/></a>
-            <a href="https://www.instagram.com/vasylynyukivan" title="instagram"><img src="./public/ins.JPG"/></a>
-            <a href="https://github.com/jvin08" title="githubtitle"><img src="./public/ghb.JPG"/></a>
+            <a href="https://twitter.com/jvin08" title="twitter"><img src="./xtw.JPG"/></a>
+            <a href="https://www.facebook.com/Djon.Pypkin/"title="meta"><img src="./f.JPG"/></a>
+            <a href="https://www.instagram.com/vasylynyukivan" title="instagram"><img src="./ins.JPG"/></a>
+            <a href="https://github.com/jvin08" title="githubtitle"><img src="./ghb.JPG"/></a>
         </div>
     )
 }
