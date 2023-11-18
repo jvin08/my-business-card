d2cb2bcd232489bdff1a2f16bd7be90a83630db7
Ivan Vasylyniuk <jvin08@gmail.com>
2023-11-17 22:55:13 -0600

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <a href={`mailto:line53inc@gmail.com`} className="btn email-btn"><img src="./icon_mail.png" className='icon-mail' alt='mail'/>Email</a>
-            <a href="/" className="btn in-btn"><img src='./in.png' className="icon-in" alt='linkedin'/>LinkedIn</a>
+            <a href="https://www.linkedin.com/in/jvin08/" className="btn in-btn"><img src='./in.png' className="icon-in" alt='linkedin'/>LinkedIn</a>
         </div>
     )
 }
